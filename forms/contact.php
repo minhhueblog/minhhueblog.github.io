34708ec9cf5b058116678f955afe9bc3d50ae963
eccedentesiast-kid <kid.nguyenlenganha@gmail.com>
2024-01-15 00:10:39 +0100

    full homepage

--- forms/contact.php
+++ forms/contact.php
@@ -12,7 +12,8 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+//    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to send the message!');
   }
 
   $contact = new PHP_Email_Form;
